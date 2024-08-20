198136ab81369a9e407ac7827d029fdeb2f06eb2
DeynerNB <deynernavarrob@gmail.com>
2024-08-20 00:14:13 -0600

    Implement set in close list

--- AStar_Algorithm.py
+++ AStar_Algorithm.py
@@ -1,7 +1,9 @@
 import numpy as np
 from numpy.typing import NDArray
 from PegSolitaire import PegSolitaire, PieceLocation, Piece, PieceInMap
-import random
+import heapq
+
+# https://en.wikipedia.org/wiki/A*_search_algorithm
 
 class AStar_Node():
     def __init__(self, game_state: NDArray, parent_node, g_value: int, h_value: int) -> None:
@@ -15,64 +17,88 @@ class AStar_Node():
 class AStar_Algorithm(PegSolitaire):
 
     def __calculateHeuristic(self, x: int, y: int):
-        return abs(3-y)+abs(3-x)
+        return abs( self.CENTER_Y - y ) + abs( self.CENTER_X - x )
 
-    def __findPossibleNextMove(self, current: AStar_Node) -> list[ AStar_Node ]:
+    def __generateNode(self, current_piece_coord: tuple[int, int], next_position_coord: tuple[int, int], current_node: AStar_Node):
+        coord_destiny_x = next_position_coord[1]
+        coord_destiny_y = next_position_coord[0]
+
+        piece_coord_x = current_piece_coord[0]
+        piece_coord_y = current_piece_coord[1]
+
+        target_piece = self.GetPieceInBetween(piece_coord_x, piece_coord_y, coord_destiny_x, coord_destiny_y)
+        target_x = target_piece[1]
+        target_y = target_piece[0]
+
+        next_game_board = self.MakeMove(piece_coord_x, piece_coord_y, coord_destiny_x, coord_destiny_y, current_node.game_state.copy())
+        if (next_game_board is not None):
+            new_gValue = current_node.g_value + 1
+            new_hValue = current_node.h_value
+
+            new_hValue -= self.__calculateHeuristic(piece_coord_x, piece_coord_y)
+            new_hValue -= self.__calculateHeuristic(target_x, target_y)
+            new_hValue += self.__calculateHeuristic(coord_destiny_x, coord_destiny_y)
+
+            new_node = AStar_Node(next_game_board.copy(), current_node, new_gValue, new_hValue)
+            return new_node
+        return None
+
+    def __findPossibleNextMove(self, current_node: AStar_Node) -> list[ AStar_Node ]:
 
         result = []
-        current_game_state = current.game_state
+        current_game_state = current_node.game_state
 
-        for y in range(current_game_state.shape[0]):
-            for x in range(current_game_state.shape[1]):
-                if current_game_state[y, x] == 1:
+        y = 0
+        x = 0
 
-                    possible_moves = self.GetPiecePossibleNextPositions(x, y)
-                    for coord in possible_moves:
+        for _ in range(self.GAME_SIZE * self.GAME_SIZE):
 
-                        coord_destiny_x = coord[1]
-                        coord_destiny_y = coord[0]
+            if current_game_state[y, x] == 1:
 
-                        target_piece = self.GetPieceInBetween(x, y, coord_destiny_x, coord_destiny_y)
-                        target_x = target_piece[1]
-                        target_y = target_piece[0]
+                possible_moves = self.GetPiecePossibleNextPositions(x, y)
 
-                        next_game_board = self.MakeMove(x, y, coord_destiny_x, coord_destiny_y, current_game_state.copy())
-                        if (next_game_board is not None):
-                            new_gValue = current.g_value + 1
-                            new_hValue = current.h_value
+                node1 = self.__generateNode( (x, y), possible_moves[0], current_node )
+                node2 = self.__generateNode( (x, y), possible_moves[1], current_node )
+                node3 = self.__generateNode( (x, y), possible_moves[2], current_node )
+                node4 = self.__generateNode( (x, y), possible_moves[3], current_node )
 
-                            new_hValue -= self.__calculateHeuristic(x, y)
-                            new_hValue -= self.__calculateHeuristic(target_x, target_y)
-                            new_hValue += self.__calculateHeuristic(coord_destiny_x, coord_destiny_y)
+                if (node1 is not None):
+                    result.append(node1)
+                if (node2 is not None):
+                    result.append(node2)
+                if (node3 is not None):
+                    result.append(node3)
+                if (node4 is not None):
+                    result.append(node4)
 
-                            new_node = AStar_Node(next_game_board.copy(), current, new_gValue, new_hValue)
-                            result.append( new_node )
+            x += 1
+            if x >= self.GAME_SIZE:
+                y += 1
+                x = 0
+
         return result
 
-    def ReconstructPath(self, current: AStar_Node):
-        pass
-    
     def rootNodeMD (self, matrix):
         m = len(matrix)
         n = len(matrix[1])
         centre = (m // 2, n // 2)
         total = 0
-        
+
         for row in range(m):
             for column in range(n):
                 if (matrix[row][column] != -1):
                     total += abs(row - centre[0]) + abs (column - centre[1])
-        
+
         print("The MD is: ", total)
         return total
-    
+
     def A_Star(self):
 
         # Set the initial and goal states
         initialState = self.GetGameMatrix()
         goalState = self.GetObjetiveMatrix()
 
-        current = None
+        current: AStar_Node = None
         initial_h = self.rootNodeMD(initialState)
         initial_g = 0
 
@@ -80,39 +106,36 @@ class AStar_Algorithm(PegSolitaire):
         openList: list[ AStar_Node ] = [
             AStar_Node(initialState, None, initial_g, initial_h),
         ]
-        closeList: list[ NDArray ] = []
+        closeList: set = set()
 
         itern = 0
         while True:
             # 1. Get the selected node based on the f_value
             # current = min(openList, key=lambda node: node.f_value)
             current = openList.pop()
-            print("Iteraciones:", itern)
             itern += 1
-            # self.PrintGame( current.game_state )
-            # print("Open list:", len(openList))
-            # print("close list:", len(closeList))
-            # print("G value:", current.g_value)
+            print("Iteraciones:", itern)
 
             # 2. Check if in the selected node, the MapState is equal to the goal
             if (np.array_equal(goalState, current.game_state)):
-                closeList.append( current )
-                self.ReconstructPath( current )
+                # closeList.add( current )
                 break
 
             # 3. Check if the game state already have been explored
-            if any(np.array_equal(current.game_state, arr) for arr in closeList):
+            state_tuple = tuple(map(tuple, current.game_state))
+            if state_tuple in closeList:
                 # openList.remove(current)
                 continue
 
             # 4. Explore next possible movement based on the current state
             nextNodes = self.__findPossibleNextMove( current )
             for node in nextNodes:
-                if not any(np.array_equal(node.game_state, arr) for arr in closeList):
+                node_tuple = tuple(map(tuple, node.game_state))
+                if node_tuple not in closeList:
                     openList.append(node)
 
             # 5. Add current to the close list and remove it from the open
-            closeList.append(current.game_state)
+            closeList.add(state_tuple)
             # openList.remove(current)
 
         foundPath = []
--- PegSolitaire.py
+++ PegSolitaire.py
@@ -9,10 +9,11 @@ PieceInMap = dict[PieceLocation, Piece]
 class PegSolitaire:
 
     GAME_SIZE = 7
+    CENTER_X = 3
+    CENTER_Y = 3
 
     def __init__(self):
         self.__initializeObjectiveMatrix()
-        # self.__initializeGameMatrix()
 
     def __initializeMatrixCorners(self, matrix: NDArray):
         # Fill the matrix with -1 in the border
@@ -44,16 +45,7 @@ class PegSolitaire:
         self.goalMatrix = self.__initializeMatrixCorners(self.goalMatrix)
 
         # Fill the matrix with 1 in the center
-        self.goalMatrix[3, 3] = 1
-
-
-    def __initializeGameMatrix(self):
-        # Create the instances of Ficha and store them in the dictionary
-        for i in range(self.goalMatrix.shape[0]):
-            for j in range(self.goalMatrix.shape[1]):
-                if self.goalMatrix[i, j] == 0:
-                    #Ficha is created with the position (i, j) in the matrix
-                    self.pieces_dict[(i, j)] = Piece(i, j)
+        self.goalMatrix[self.CENTER_Y, self.CENTER_X] = 1
 
     def PrintGame(self, game_board: NDArray):
         temp_matrix = np.zeros((self.GAME_SIZE, self.GAME_SIZE), dtype=object)
@@ -89,19 +81,16 @@ class PegSolitaire:
 
         # Check if the from location has no piece -> Cant move
         if game_board[y_from, x_from] != 1:
-            # print("Invalid move: 1")
             return None
 
         # Check if the to location has a piece -> Cant move
         if game_board[y_to, x_to] != 0:
-            # print("Invalid move: 2")
             return None
 
         # Check if the select locations are near each other (0: Pieces from same X/Y ; 2: Exact difference between X/Y for valid move)
         diff_x = abs(x_from - x_to)
         diff_y = abs(y_from - y_to)
         if (diff_x != 0 and diff_x != 2) or (diff_y != 0 and diff_y != 2):
-            # print("Invalid move: 3")
             return None
 
         # 1. Get piece in between
@@ -109,7 +98,6 @@ class PegSolitaire:
 
         # 2. Check if the target location has no piece -> Cant move
         if game_board[ targetPiece[0], targetPiece[1] ] != 1:
-            # print("Invalid move: 4")
             return None
 
         # 3. Remove the from location, target piece and set the to location
@@ -122,8 +110,8 @@ class PegSolitaire:
     def GetPiecePossibleNextPositions(self, x: int, y: int) -> list[tuple[int, int]] :
         up_location    = (y - 2, x)
         down_location  = (y + 2, x)
-        left_location  = (y, x - 2)
         right_location = (y, x + 2)
+        left_location  = (y, x - 2)
 
         return [ up_location, down_location, right_location, left_location ]
 
